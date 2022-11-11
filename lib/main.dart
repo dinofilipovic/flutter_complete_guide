d1feecbe2873fabc73b78bfffaa05395fd7b9af1
Dino FIlipović <filipovic.dino@gmail.com>
2022-11-11 10:57:56 +0100

    Update main.dart with AppBar and Body
    - added AppBar and updated Confluence page
    - added body and Text widget

--- lib/main.dart
+++ lib/main.dart
@@ -5,9 +5,15 @@ void main() {
 }
 
 class MyApp extends StatelessWidget {
+  @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      home: Text('Hello'),
+      home: Scaffold(
+        appBar: AppBar(
+          title: Text('My First Application'),
+        ),
+        body: Text('This is my Defult text!'),
+      ),
     );
   }
 }
