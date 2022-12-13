c4c20acb57400f00756317d65bf9391f6b4c65e0
Dino FIlipović <filipovic.dino@gmail.com>
2022-12-13 19:24:11 +0100

    Split main.dart into several widgets
    Created:
    - quiz.dart
    - Holds and operates with quiz questions
    - result.dart
    - result information

--- lib/main.dart
+++ lib/main.dart
@@ -1,7 +1,6 @@
 import 'package:flutter/material.dart';
 
-import './question.dart';
-import './answer.dart';
+import './quiz.dart';
 
 void main() {
   runApp(MyApp());
@@ -21,7 +20,7 @@ class MyApp extends StatefulWidget {
 class _MyAppState extends State<MyApp> {
   //Had to change it so I can set up conditionals in the code because const
 //is being used and it can be set up during compile phase
-  final questions = const [
+  final _questions = const [
     {
       'questionText': 'What\'s your favorite color?',
       'answers': ['Black', 'Red', 'Green', 'White']
@@ -39,7 +38,7 @@ class _MyAppState extends State<MyApp> {
   var _questionIndex = 0;
   // _MyAppState Method which operates question index in the application
   void _answerQuestion() {
-    if (_questionIndex < questions.length) {}
+    if (_questionIndex < _questions.length) {}
     setState(() {
       _questionIndex += 1;
     });
@@ -52,17 +51,15 @@ class _MyAppState extends State<MyApp> {
         appBar: AppBar(
           title: const Text('My First Application'),
         ),
-        body: Column(
-          children: [
-            Question(
-              questions[_questionIndex]['questionText'] as String,
-            ),
-            ...(questions[_questionIndex]['answers'] as List<String>)
-                .map((answer) {
-              return Answer(_answerQuestion, answer);
-            }).toList(),
-          ],
-        ),
+        body: _questionIndex < _questions.length
+            ? Quiz(
+                answerQuestion: _answerQuestion,
+                questions: _questions,
+                questionIndex: _questionIndex,
+              )
+            : Center(
+                child: Text('You did it!'),
+              ),
       ),
     );
   }
